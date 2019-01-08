f0a83c131c252a255233fc54939fac5a4981c491
775371 <44507768+775371@users.noreply.github.com>
2019-01-08 12:20:16 +0800

    Update func_table.h

--- src/func_table.h
+++ src/func_table.h
@@ -31,9 +31,10 @@ extern int CTinit(int n, double *y[], int maxcat, char **error,
 
 /* beta*/
 extern void CTss(int n, double *y[], double *value, double *tr_mean, double *con_mean, double *risk, 
-                 double *wt, double *treatment, double *treatments, double max_y, double alpha, double train_to_est_ratio);
+                 double *wt, double *treatment, double *treatments, double max_y, double alpha, double eta, double train_to_est_ratio);
 extern void CT(int n, double *y[], double *x, int nclass,int edge, double *improve, double *split, 
                int *csplit, double myrisk, double *wt, double *treatment, double *treatments, int minsize, double alpha,
+	       double eta,
                double train_to_est_ratio);
 
 
@@ -44,10 +45,10 @@ extern int CTDinit(int n, double *y[], int maxcat, char **error, int *size,
                    double *train_to_est_ratio);
 
 extern void CTDss(int n, double *y[], double *value, double *tr_mean, double *con_mean, double *risk, double *wt, 
-                  double *treatment, double max_y, double alpha, double train_to_est_ratio);
+                  double *treatment, double max_y, double alpha, double eta, double train_to_est_ratio);
 extern void CTD(int n, double *y[], double *x, int nclass,
         int edge, double *improve, double *split, int *csplit,
-        double myrisk, double *wt, double *treatment, int minsize, double alpha, int bucketnum,
+        double myrisk, double *wt, double *treatment, int minsize, double alpha, double eta, int bucketnum,
         int bucketMax, double train_to_est_ratio);
 extern double CTDpred(double *y, double wt, double treatment, double *yhat, 
                       double propensity); 
