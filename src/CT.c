b7d45a249d492a2eb5f82979ae2ec1b5928b02b9
775371 <44507768+775371@users.noreply.github.com>
2019-01-20 11:25:26 -0500

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -484,20 +484,23 @@ void CT(int n, double *y[], double *x, int nclass, int edge, double *improve, do
                 
         }
         
-        for (i = 0; i < nclass; i++) {
-            if (countn[i] > 0) {
+        for (i = 0; i < nclass; i++) {Rprintf("nclass in function CT in CT.c is %d\n", nclass);
+				      
+            if (countn[i] > 0) {Rprintf("countn in function CT in CT.c is %d\n", countn); 
+				
+				
                 tsplit[i] = RIGHT;
                //treatment_effect[i] = trsums[j] / trs[j] - (wtsums[j] - trsums[j]) / (wts[j] - trs[j]);
                     
-      treatment_effect[i]=  ((countn[i]* yz_[i]*countn[i]* kk_[i] - countn[i]* yz_[i] * k_[i] * k_[i] -
+      treatment_effect[i] =  ((countn[i]* yz_[i]*countn[i]* kk_[i] - countn[i]* yz_[i] * k_[i] * k_[i] -
       y_[i] * z_[i] *countn[i]* kk_[i] + y_[i] * z_[i] * k_[i] * k_[i])-(countn[i]* kz_[i] *countn[i]* ky_[i]-
       countn[i]* kz_[i] * y_[i] * k_[i] - z_[i] * k_[i] *countn[i]* ky_[i] + z_[i] * k_[i] * k_[i] * y_[i])) 
-      / ( (countn[i] * yy_[i] - y_[i] * y_[i]) * (countn[i]* kk_[i]- k_[i] * k_[i]) - (countn[i] * ky_[i] - yy_[i] * kk_[i])); 
+      /( (countn[i] * yy_[i] - y_[i] * y_[i]) * (countn[i]* kk_[i]- k_[i] * k_[i]) - (countn[i] * ky_[i] - yy_[i] * kk_[i])); 
 
 		    
-treatments_effect[i]=  ((countn[i]* kz_[i] *countn[i]* yy_[i]-countn[i]* kz_[i] * y_[i] * y_[i]- z_[i] * k_[i] *countn[i]*yy_[i] + z_[i] * k_[i] * y_[i] * y_[i])
+treatments_effect[i] =  ((countn[i]* kz_[i] *countn[i]* yy_[i]-countn[i]* kz_[i] * y_[i] * y_[i]- z_[i] * k_[i] *countn[i]*yy_[i] + z_[i] * k_[i] * y_[i] * y_[i])
               -(countn[i]* yz_[i] *countn[i]* ky_[i] -countn[i]* yz_[i] * y_[i] *k_[i] - z_[i] * y_[i] *countn[i]* ky_[i] + z_[i] * y_[i] * y_[i] * k_[i])) 
-            / ( (countn[i]* yy_[i] - y_[i] * y_[i])*(countn[i]* kk_[i] - k_[i] * k_[i])-(countn[i]*ky_[i]-yy_[i]*kk_[i]) );
+            /( (countn[i]* yy_[i] - y_[i] * y_[i])*(countn[i]* kk_[i] - k_[i] * k_[i])-(countn[i]*ky_[i]-yy_[i]*kk_[i]) );
 
                            
            Rprintf("treatment_effect in CT.c %d.\n", treatment_effect);                
